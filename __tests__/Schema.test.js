45ea5773a6078856fb73836e684137a6431466ed
Joe Klause <joeklause@gmail.com>
2019-09-17 18:08:45 -0700

    starting to build schema, pulling out schema piece

--- __tests__/Schema.test.js
+++ __tests__/Schema.test.js
@@ -1,16 +1,52 @@
 const Schema = require('../lib/Schema');
 
-describe('Schema', () => {
+const validModel = {
+  'firstName': 'Chris',
+  'lastName': 'Sample',
+  'hair': {
+    'type': 'wavy',
+    'color': 'brown'
+  },
+  'favoriteFoods': [
+    'pizza',
+    'cupcakes',
+    'salmon'
+  ],
+  'married': true,
+  'kids': 3
+};
 
-  // add a test schema
+const invalidModel = {
+  'firstName': '',
+  'lastName': 0,
+  'hair': {
+    'type': 'wavy',
+    'color': 'brown'
+  },
+  'favoriteFoods': [
+    'pizza',
+    'cupcakes',
+    'salmon'
+  ],
+  'married': true,
+  'kids': 3
+};
+
+describe('Schema', () => {
+  const personSchema = {
+    firstName: { type: 'string', required: true },
+    lastName: { type: 'string', required: true },
+    married: { type: 'boolean', required: true },
+    kids: { type: 'number', required: true }
+  };
 
   it('validates a correct model', () => {
-
+    const schema = new Schema(personSchema);
+    schema.validate(validModel);
   });
 
   it('throws on invalid model', () => {
-
+    const schema = new Schema(personSchema);
+    schema.validate(invalidModel);
   });
-
-  // more test cases...
 });
