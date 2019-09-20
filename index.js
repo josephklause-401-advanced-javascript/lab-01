8daec8f833788768d23505b45f1631e2e126fca0
Joe Klause <joeklause@gmail.com>
2019-09-19 17:14:47 -0700

    setting up lab-04

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-
 const validator = require('./lib/validator.js');
+const DocumentCollection = require('./lib/document-collection');
+
 
 console.log(validator.isString('hello world'));
+
+const documents = new DocumentCollection('destination.txt');
+
+const testObject = {
+  test: 42,
+  hair: 'purple'
+};
+
+documents.save(testObject)
+  .then(obj => {
+    console.log('save', obj);
+
+    documents.get(obj.id)
+      .then(obj => {
+        console.log('get', obj);
+
+        documents.getAll()
+          .then(obj => {
+            console.log('getAll', obj);
+          });
+      });
+  });
